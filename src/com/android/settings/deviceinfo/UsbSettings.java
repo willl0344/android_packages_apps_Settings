cf2aafa04cdca1037fc45ca859ea30de5b58cbf4
Janson Kang <temasek71@gmail.com>
2012-12-17 11:46:08 +0800

    Revert "UMS support with any volume, not only primary (3/3)"
    This reverts commit 8a09e4ccd9fcc31898fff6d643e2f2a449339910.

--- src/com/android/settings/deviceinfo/UsbSettings.java
+++ src/com/android/settings/deviceinfo/UsbSettings.java
@@ -72,10 +72,6 @@ public class UsbSettings extends SettingsPreferenceFragment {
 
         mMtp = (CheckBoxPreference)root.findPreference(KEY_MTP);
         mPtp = (CheckBoxPreference)root.findPreference(KEY_PTP);
-        mUms = (CheckBoxPreference)root.findPreference(KEY_MASS_STORAGE);
-        if (!storageManager.isUsbMassStorageSupported()) {
-            root.removePreference(mUms);
-        }
 
         return root;
     }
