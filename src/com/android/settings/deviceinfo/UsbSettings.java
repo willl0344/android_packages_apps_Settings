c0f94d65082cac2743d8f515c6a9b676aaf129e5
Janson Kang <temasek71@gmail.com>
2013-01-25 08:27:10 +0800

    UMS support with any volume, not only primary

--- src/com/android/settings/deviceinfo/UsbSettings.java
+++ src/com/android/settings/deviceinfo/UsbSettings.java
@@ -79,7 +79,7 @@ public class UsbSettings extends SettingsPreferenceFragment {
         mMtp = (CheckBoxPreference)root.findPreference(KEY_MTP);
         mPtp = (CheckBoxPreference)root.findPreference(KEY_PTP);
         mUms = (CheckBoxPreference)root.findPreference(KEY_MASS_STORAGE);
-        if (!storageVolumes[0].allowMassStorage()) {
+        if (!storageManager.isUsbMassStorageSupported()) {
             root.removePreference(mUms);
         }
 
