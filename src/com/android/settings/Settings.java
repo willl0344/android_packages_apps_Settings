585b0991f5ff6d93cce0b9fda6aa7db9f4d0c457
corcoran <xxx@xxx>
2013-04-24 20:22:39 +0800

    Settings: Notification Shortcuts (2/2) Patchset 2: Added hide carrier label toggle, added multi-user support, fixed whitespace, cleanup of code.
    Change-Id: I4e6c460289f5375acb419b4f980be15b290d8596

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -893,4 +893,5 @@ public class Settings extends PreferenceActivity
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class SystemSettingsActivity extends Settings { /* empty */ }
+    public static class NotificationShortcutsSettingsActivity extends Settings { /* empty */ }
 }
