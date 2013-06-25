836f9010562bc6e9f7a7e72e80ae33e86fbbdeb3
Janson Kang <temasek71@gmail.com>
2013-06-25 21:04:25 +0800

    Comment out removal of hardware keys

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -71,55 +71,55 @@ public class SystemSettings extends SettingsPreferenceFragment  implements
 
         // Only show the hardware keys config on a device that does not have a navbar
         // and the navigation bar config on phones that has a navigation bar
-        boolean removeKeys = false;
-        boolean removeNavbar = false;
+//        boolean removeKeys = false;
+//        boolean removeNavbar = false;
 
-        IWindowManager windowManager = IWindowManager.Stub.asInterface(
-                ServiceManager.getService(Context.WINDOW_SERVICE));
-        try {
-            if (windowManager.hasNavigationBar()) {
-                removeKeys = true;
-            } else {
-                removeNavbar = true;
-            }
-        } catch (RemoteException e) {
-            // Do nothing
-        }
+//        IWindowManager windowManager = IWindowManager.Stub.asInterface(
+//                ServiceManager.getService(Context.WINDOW_SERVICE));
+//        try {
+//            if (windowManager.hasNavigationBar()) {
+//                removeKeys = true;
+//            } else {
+//                removeNavbar = true;
+//            }
+//        } catch (RemoteException e) {
+//            // Do nothing
+//        }
 
-        // Determine which user is logged in
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
-        if (mIsPrimary) {
-            // Primary user only preferences
-            // Battery lights
-            mBatteryPulse = (PreferenceScreen) findPreference(KEY_BATTERY_LIGHT);
-            if (mBatteryPulse != null) {
-                if (getResources().getBoolean(
-                        com.android.internal.R.bool.config_intrusiveBatteryLed) == false) {
-                    prefScreen.removePreference(mBatteryPulse);
-                    mBatteryPulse = null;
-                }
-            }
+//        // Determine which user is logged in
+//        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+//        if (mIsPrimary) {
+//            // Primary user only preferences
+//            // Battery lights
+//            mBatteryPulse = (PreferenceScreen) findPreference(KEY_BATTERY_LIGHT);
+//            if (mBatteryPulse != null) {
+//                if (getResources().getBoolean(
+//                        com.android.internal.R.bool.config_intrusiveBatteryLed) == false) {
+//                    prefScreen.removePreference(mBatteryPulse);
+//                    mBatteryPulse = null;
+//                }
+//            }
 
-            // Act on the above
-            if (removeKeys) {
-                prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
-            }
-            if (removeNavbar) {
-                prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
-                prefScreen.removePreference(findPreference(KEY_NAVIGATION_RING));
-                prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR_CATEGORY));
-            }
-        } else {
-            // Secondary user is logged in, remove all primary user specific preferences
-            prefScreen.removePreference(findPreference(KEY_BATTERY_LIGHT));
-            prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
-            prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
-            prefScreen.removePreference(findPreference(KEY_NAVIGATION_RING));
-            prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR_CATEGORY));
-            prefScreen.removePreference(findPreference(KEY_STATUS_BAR));
-            prefScreen.removePreference(findPreference(KEY_QUICK_SETTINGS));
-            prefScreen.removePreference(findPreference(KEY_POWER_MENU));
-            prefScreen.removePreference(findPreference(KEY_NOTIFICATION_DRAWER));
+//            // Act on the above
+//            if (removeKeys) {
+//                prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
+//            }
+//            if (removeNavbar) {
+//                prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
+//                prefScreen.removePreference(findPreference(KEY_NAVIGATION_RING));
+//                prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR_CATEGORY));
+//            }
+//        } else {
+//            // Secondary user is logged in, remove all primary user specific preferences
+//            prefScreen.removePreference(findPreference(KEY_BATTERY_LIGHT));
+//            prefScreen.removePreference(findPreference(KEY_HARDWARE_KEYS));
+//            prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR));
+//            prefScreen.removePreference(findPreference(KEY_NAVIGATION_RING));
+//            prefScreen.removePreference(findPreference(KEY_NAVIGATION_BAR_CATEGORY));
+//            prefScreen.removePreference(findPreference(KEY_STATUS_BAR));
+//            prefScreen.removePreference(findPreference(KEY_QUICK_SETTINGS));
+//            prefScreen.removePreference(findPreference(KEY_POWER_MENU));
+//            prefScreen.removePreference(findPreference(KEY_NOTIFICATION_DRAWER));
         }
 
         // Preferences that applies to all users
