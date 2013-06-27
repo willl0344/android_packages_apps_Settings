ae4fe4092b57868c21c20c15d262af256126413d
Janson Kang <temasek71@gmail.com>
2013-06-27 09:28:08 +0800

    InstalledAppDetails.java: Remove Privacy Guard

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -994,7 +994,6 @@ public class InstalledAppDetails extends Fragment
             initDataButtons();
             initMoveButton();
             initNotificationButton();
-            initPrivacyGuardButton();
         } else {
             mMoveAppButton.setText(R.string.moving);
             mMoveAppButton.setEnabled(false);
