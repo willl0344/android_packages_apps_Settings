9b78133a63f0966d37ff807cfddc6d2d55eb21b5
Janson Kang <temasek71@gmail.com>
2013-08-04 22:15:40 +0800

    Revert "Nav Bar Width in landscape (realtime change)"
    This reverts commit 57f6ba1d3cc784b0e16f058424e25dc817e7cacc.
    
    Conflicts:
    
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -61,7 +61,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     // If there is no setting in the provider, use this
     private static final int FALLBACK_SCREEN_TIMEOUT_VALUE = 30000;
     private static final int FALLBACK_ON_SCREEN_BUTTONS_HEIGHT = 32;
-    private static final int FALLBACK_ON_SCREEN_BUTTONS_WIDTH = 32;
 
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
     private static final String KEY_FONT_SIZE = "font_size";
@@ -75,7 +74,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final String ON_SCREEN_BUTTONS_HEIGHT = "on_screen_buttons_height";
-    private static final String ON_SCREEN_BUTTONS_WIDTH = "on_screen_buttons_width";
+    //private static final String ON_SCREEN_BUTTONS_WIDTH = "on_screen_buttons_width";
     private static final String KEY_DUAL_PANEL = "force_dualpanel"; 
 
     // Strings used for building the summary
@@ -98,7 +97,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private WarnedListPreference mFontSizePref;
 
     private ListPreference mOnScreenButtonsHeight;
-    private ListPreference mOnScreenButtonsWidth;
 
     private final Configuration mCurConfig = new Configuration();
 
@@ -164,11 +162,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         final long currentOnScreenButtonsHeight = Settings.System.getLong(resolver, ON_SCREEN_BUTTONS_HEIGHT, FALLBACK_ON_SCREEN_BUTTONS_HEIGHT);
         mOnScreenButtonsHeight.setValue(String.valueOf(currentOnScreenButtonsHeight));
         mOnScreenButtonsHeight.setOnPreferenceChangeListener(this);
-
-        mOnScreenButtonsWidth = (ListPreference) findPreference(ON_SCREEN_BUTTONS_WIDTH);
-        final long currentOnScreenButtonsWidth = Settings.System.getLong(resolver, ON_SCREEN_BUTTONS_WIDTH, FALLBACK_ON_SCREEN_BUTTONS_WIDTH);
-        mOnScreenButtonsWidth.setValue(String.valueOf(currentOnScreenButtonsWidth));
-        mOnScreenButtonsWidth.setOnPreferenceChangeListener(this);
 
         mFontSizePref = (WarnedListPreference) findPreference(KEY_FONT_SIZE);
         mFontSizePref.setOnPreferenceChangeListener(this);
@@ -506,15 +499,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.putInt(getContentResolver(), ON_SCREEN_BUTTONS_HEIGHT, value);
             } catch (NumberFormatException e) {
                 Log.e(TAG, "could not persist on-screen button height setting", e);
-            }
-        }
-
-        if (ON_SCREEN_BUTTONS_WIDTH.equals(key)) {
-            int value = Integer.parseInt((String) objValue);
-            try {
-                Settings.System.putInt(getContentResolver(), ON_SCREEN_BUTTONS_WIDTH, value);
-            } catch (NumberFormatException e) {
-                Log.e(TAG, "could not persist on-screen button width setting", e);
             }
         }
 
