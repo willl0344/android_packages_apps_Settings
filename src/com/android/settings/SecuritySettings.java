4e2db0e6b7124351f0d7068ff2321346438031a4
Janson Kang <temasek71@gmail.com>
2013-10-13 10:36:42 +0800

    Revert "Settings: Security: implement a pin keypad shuffler [2/2]"
    This reverts commit ad1ccd4b880fb67b03a3f686496563e20ae66ae0.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -101,9 +101,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_BLACKLIST = "blacklist";
     private static final String CATEGORY_ADDITIONAL = "additional_options";
 
-    // Slim Additions
-    private static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
-
     private PackageManager mPM;
     DevicePolicyManager mDPM;
 
@@ -138,9 +135,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private ListPreference mSlideLockScreenOffDelay;
     private ListPreference mSmsSecurityCheck;
     private PreferenceScreen mBlacklist;
-
-    // Slim Additions
-    private ListPreference mLockNumpadRandom;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -298,7 +292,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 menuUnlock.setEnabled(false);
                 homeUnlock.setEnabled(false);
                 vibratePref.setEnabled(false);
-                mLockNumpadRandom.setEnabled(false);
             // disable menu unlock and vibrate on unlock options if
             // using PIN/password as primary lock screen or as
             // backup to biometric
@@ -307,12 +300,10 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 menuUnlock.setEnabled(false);
                 homeUnlock.setEnabled(false);
                 vibratePref.setEnabled(false);
-                mLockNumpadRandom.setEnabled(mLockPatternUtils.isLockNumericPasswordEnabled());
             // Disable the quick unlock if its not using PIN/password
             // as a primary lock screen or as a backup to biometric
             } else {
                 quickUnlockScreen.setEnabled(false);
-                mLockNumpadRandom.setEnabled(false);
             }
 
             final int deviceKeys = res.getInteger(
@@ -432,13 +423,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
                     mToggleVerifyApps.setEnabled(false);
                 }
             }
-
-            // Lock Numpad Random
-            mLockNumpadRandom = (ListPreference) root.findPreference(LOCK_NUMPAD_RANDOM);
-            mLockNumpadRandom.setValue(String.valueOf(Settings.Secure.getInt(resolver,
-                    Settings.Secure.LOCK_NUMPAD_RANDOM, 0)));
-            mLockNumpadRandom.setSummary(mLockNumpadRandom.getEntry());
-            mLockNumpadRandom.setOnPreferenceChangeListener(this);
 
             // App security settings
             addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
@@ -831,12 +815,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
             Settings.Global.putInt(getContentResolver(), Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT,
                      smsSecurityCheck);
             updateSmsSecuritySummary(smsSecurityCheck);
-        } else if (preference == mLockNumpadRandom) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.LOCK_NUMPAD_RANDOM,
-                    Integer.valueOf((String) value));
-            mLockNumpadRandom.setValue(String.valueOf(value));
-            mLockNumpadRandom.setSummary(mLockNumpadRandom.getEntry());
         }
         return true;
     }
