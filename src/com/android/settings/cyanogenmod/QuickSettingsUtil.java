ad8f1ba78f3c16686e121fba174ad81da839e040
Janson Kang <temasek71@gmail.com>
2013-10-30 15:39:39 +0800

    Revert "Revert "[Settings] Add reboot tile to QS part2""
    This reverts commit 0d757567f0975944e9d14bf0aedafe4d77e8f001.

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -44,6 +44,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
+import static com.android.internal.util.cm.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.cm.QSConstants.TILE_HALO;
 import static com.android.internal.util.cm.QSConstants.TILE_POWER;
 
@@ -148,6 +149,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_WIFIAP, R.string.title_tile_wifiap,
                 "com.android.systemui:drawable/ic_qs_wifi_ap_neutral"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_REBOOT, R.string.title_tile_reboot,
+                "com.android.systemui:drawable/ic_qs_reboot"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NETWORKADB, R.string.title_tile_network_adb,
                 "com.android.systemui:drawable/ic_qs_network_adb_off"));
