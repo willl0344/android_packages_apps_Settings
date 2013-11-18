5cf606699582b7b37c36e2361d3c0c2869b5f68e
Janson Kang <temasek71@gmail.com>
2013-11-18 10:57:53 +0800

    Settings: MediaScanner behavior on boot (3/3) Lets you enable/disable/ask (with a notification) media scanning on boot. Can speed up boot and reduce initial boot lag from MediaScanner
    Credit: SlimROM

--- src/com/android/settings/temasek/GeneralSettings.java
+++ src/com/android/settings/temasek/GeneralSettings.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
@@ -22,10 +23,12 @@ public class GeneralSettings extends SettingsPreferenceFragment implements
     private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
     private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
     private static final String RESTART_SYSTEMUI = "restart_systemui";
+    private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
     private CheckBoxPreference mUseAltResolver;
     private CheckBoxPreference mShowCpuInfo;
     private Preference mRestartSystemUI;
+    private ListPreference mMsob;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,12 @@ public class GeneralSettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVITY_RESOLVER_USE_ALT, 0) == 1);
 
         mRestartSystemUI = findPreference(RESTART_SYSTEMUI);
+
+        mMsob = (ListPreference) findPreference(PREF_MEDIA_SCANNER_ON_BOOT);
+        mMsob.setValue(String.valueOf(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
+        mMsob.setSummary(mMsob.getEntry());
+        mMsob.setOnPreferenceChangeListener(this);
 
     }
 
@@ -78,7 +87,18 @@ public class GeneralSettings extends SettingsPreferenceFragment implements
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String value = (String) newValue;
+        if (preference == mMsob) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.MEDIA_SCANNER_ON_BOOT,
+                    Integer.valueOf(value));
+
+            mMsob.setValue(String.valueOf(value));
+            mMsob.setSummary(mMsob.getEntry());
+            return true;
+        }
         return false;
     }
 }
