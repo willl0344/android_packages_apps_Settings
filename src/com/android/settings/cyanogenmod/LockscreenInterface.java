5b05b547fc82e0b9b23ea1a35440055ec74c62e2
Janson Kang <temasek71@gmail.com>
2013-11-19 09:54:07 +0800

    Revert "Settings : Port lockscreen shortcut configuration (WIP)"
    This reverts commit 01777c354f609dd5b482b72ca38aad02b741d27a.

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -121,15 +121,13 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
         final LockPatternUtils lockPatternUtils = mChooseLockSettingsHelper.utils();
         if (KEY_ENABLE_WIDGETS.equals(key)) {
             lockPatternUtils.setWidgetsEnabled(mEnableKeyguardWidgets.isChecked());
-            return true;
         } else if (preference == mLockRingBattery) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.BATTERY_AROUND_LOCKSCREEN_RING, mLockRingBattery.isChecked()
                     ? 1 : 0);
-            return true;
         }
 
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+        return true;
     }
 
     public static class DeviceAdminLockscreenReceiver extends DeviceAdminReceiver {}
