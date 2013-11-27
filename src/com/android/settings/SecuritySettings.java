thefutureosp/android_packages_apps_Settings-2

Settings: Port Lockscreen Targets and Shortcuts
    First thanks to PA and CM which gave us partly code and some ideas.
    Still some parts of the original implementation are/were buggy as well
    we needed to ehance it.

    Whatever the new Slim version is now ready and seems to be completly bug
    free.

    - Configurable lockscreen targets...on tablets 8 on phone either 5 or 8
    - configurable lockscreen shortcuts only on phones...up to 5 shortcuts
    - Centralazing some methods....using now then rounded corner on custom
      images systemwide...means on all other shorcut features we have
    - multiDpi compatible
    - new settings logic
    - instance handling of all dialogs like we do on all other places
      way nicer this way
    - tons of fixes

    thanks kroz for redoing all xxhdpi icon in fb

Change-Id: I74d4405e6eed0cd79baa32e3fd197bd36f3f3c8c

src/com/android/settings/SecuritySettings.java
@@ -44,6 +44,7 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import com.android.internal.telephony.util.BlacklistUtils;
+import com.android.internal.util.crdroid.DeviceUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
 
@@ -68,6 +69,9 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_DEVICE_ADMIN_CATEGORY = "device_admin_category";
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
+    private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
+    private static final String KEY_INTERFACE_SETTINGS = "lock_screen_settings";
+    private static final String KEY_TARGET_SETTINGS = "lockscreen_targets";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -110,7 +114,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private DialogInterface mWarnInstallApps;
     private CheckBoxPreference mToggleVerifyApps;
     private CheckBoxPreference mPowerButtonInstantlyLocks;
-
 
     private Preference mNotificationAccess;
 
@@ -180,7 +183,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
         }
         addPreferencesFromResource(resid);
 
-
         // Add options for device encryption
         mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
@@ -227,12 +229,25 @@ public class SecuritySettings extends RestrictedSettingsFragment
         mPowerButtonInstantlyLocks = (CheckBoxPreference) root.findPreference(
                 KEY_POWER_INSTANTLY_LOCKS);
 
+        PreferenceGroup securityCategory = (PreferenceGroup)
+                root.findPreference(KEY_SECURITY_CATEGORY);
+        if (securityCategory != null) {
+            Preference lockInterfacePref = findPreference(KEY_INTERFACE_SETTINGS);
+            Preference lockTargetsPref = findPreference(KEY_TARGET_SETTINGS);
+            if (lockInterfacePref != null && lockTargetsPref != null) {
+                if (!DeviceUtils.isPhone(getActivity())) {
+                     // Nothing for tablets and large screen devices
+                     securityCategory.removePreference(lockInterfacePref);
+                } else {
+                     securityCategory.removePreference(lockTargetsPref);
+                }
+            }
+        }
+
         // don't display visible pattern if biometric and backup is not pattern
         if (resid == R.xml.security_settings_biometric_weak &&
                 mLockPatternUtils.getKeyguardStoredPasswordQuality() !=
                 DevicePolicyManager.PASSWORD_QUALITY_SOMETHING) {
-            PreferenceGroup securityCategory = (PreferenceGroup)
-                    root.findPreference(KEY_SECURITY_CATEGORY);
             if (securityCategory != null && mVisiblePattern != null) {
                 securityCategory.removePreference(root.findPreference(KEY_VISIBLE_PATTERN));
             }
@@ -254,7 +269,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
                 root.findPreference(KEY_SIM_LOCK).setEnabled(false);
             }
         }
-
 
         // Show password
         mShowPassword = (CheckBoxPreference) root.findPreference(KEY_SHOW_PASSWORD);
@@ -516,7 +530,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
 
-        updateBlacklistSummary();
+	updateBlacklistSummary();
     }
 
     @Override
@@ -567,7 +581,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
             lockPatternUtils.setVisiblePatternEnabled(isToggled(preference));
         } else if (KEY_POWER_INSTANTLY_LOCKS.equals(key)) {
             lockPatternUtils.setPowerButtonInstantlyLocks(isToggled(preference));
-        } else if (preference == mShowPassword) {
+	} else if (preference == mShowPassword) {
             Settings.System.putInt(getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD,
                     mShowPassword.isChecked() ? 1 : 0);
         } else if (preference == mToggleAppInstallation) {

src/com/android/settings/crdroid/dslv/ButtonsListViewSettings.java
@@ -55,7 +55,7 @@ import android.widget.Toast;
 import com.android.internal.util.crdroid.ButtonConfig;
 import com.android.internal.util.crdroid.ButtonsConstants;
 import com.android.internal.util.crdroid.ButtonsHelper;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.internal.util.crdroid.DeviceUtils;
 import com.android.internal.util.crdroid.DeviceUtils.FilteredDeviceFeaturesArray;
 import com.android.internal.util.crdroid.PolicyHelper;
@@ -197,7 +197,7 @@ public class ButtonsListViewSettings extends ListFragment implements
         mActionDialogValues = finalActionDialogArray.values;
         mActionDialogEntries = finalActionDialogArray.entries;
 
-        mPicker = new ShortcutPickerHelper(this, this);
+        mPicker = new ShortcutPickerHelper(mActivity, this);
 
         mImageTmp = new File(mActivity.getCacheDir()
                 + File.separator + "shortcut.tmp");
@@ -218,7 +218,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                         mPendingIndex = arg2;
                         mPendingLongpress = false;
                         mPendingNewButton = false;
-                        mPicker.pickShortcut();
+                        mPicker.pickShortcut(getId());
                     }
                 }
             }
@@ -236,7 +236,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                             mPendingIndex = arg2;
                             mPendingLongpress = true;
                             mPendingNewButton = false;
-                            mPicker.pickShortcut();
+                            mPicker.pickShortcut(getId());
                         }
                     }
                     return true;
@@ -290,7 +290,7 @@ public class ButtonsListViewSettings extends ListFragment implements
 
     @Override
     public void shortcutPicked(String action,
-                String description, Bitmap bmp, boolean isApplication) {
+                String description, boolean isApplication) {
         if (mPendingIndex == -1) {
             return;
         }
@@ -419,7 +419,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                         mPendingIndex = 0;
                         mPendingLongpress = false;
                         mPendingNewButton = true;
-                        mPicker.pickShortcut();
+                        mPicker.pickShortcut(getId());
                     }
                 }
                 break;
@@ -473,6 +473,8 @@ public class ButtonsListViewSettings extends ListFragment implements
             case POWER_MENU_SHORTCUT:
                 return PolicyHelper.getPowerMenuConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
+            case LOCKSCREEN_SHORTCUT:
+                return ButtonsHelper.getLockscreenShortcutConfig(mActivity);
         }
         return null;
     }
@@ -493,6 +495,8 @@ public class ButtonsListViewSettings extends ListFragment implements
                 break;
             case POWER_MENU_SHORTCUT:
                 PolicyHelper.setPowerMenuConfig(mActivity, buttonConfigs, reset);
+            case LOCKSCREEN_SHORTCUT:
+                ButtonsHelper.setLockscreenShortcutConfig(mActivity, buttonConfigs, reset);
                 break;
         }
     }
@@ -538,12 +542,12 @@ public class ButtonsListViewSettings extends ListFragment implements
                     + " " + getItem(position).getLongpressActionDescription());
             }
             if (mButtonMode == POWER_MENU_SHORTCUT) {
-                holder.iconView.setImageDrawable(ColorHelper.resize(
+                holder.iconView.setImageDrawable(ImageHelper.resize(
                         mActivity, PolicyHelper.getPowerMenuIconImage(mActivity,
                         getItem(position).getClickAction(),
                         getItem(position).getIcon(), false), 36));
             } else {
-                holder.iconView.setImageDrawable(ColorHelper.resize(
+                holder.iconView.setImageDrawable(ImageHelper.resize(
                         mActivity, ButtonsHelper.getButtonIconImage(mActivity,
                         getItem(position).getClickAction(),
                         getItem(position).getIcon()), 36));
@@ -729,7 +733,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                                     getOwner().mPendingIndex = which;
                                     getOwner().mPendingLongpress = longpress;
                                     getOwner().mPendingNewButton = newButton;
-                                    getOwner().mPicker.pickShortcut();
+                                    getOwner().mPicker.pickShortcut(getId());
                                 }
                             } else {
                                 if (newButton) {

src/com/android/settings/crdroid/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.crdroid.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 0 : 1);
+        }
+        return true;
+    }
+}

willl0344/android_packages_apps_Settings

src/com/android/settings/SecuritySettings.java
@@ -44,6 +44,7 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import com.android.internal.telephony.util.BlacklistUtils;
+import com.android.internal.util.temasek.DeviceUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
 
@@ -68,6 +69,9 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_DEVICE_ADMIN_CATEGORY = "device_admin_category";
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
+    private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
+    private static final String KEY_INTERFACE_SETTINGS = "lock_screen_settings";
+    private static final String KEY_TARGET_SETTINGS = "lockscreen_targets";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -113,7 +117,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private DialogInterface mWarnInstallApps;
     private CheckBoxPreference mToggleVerifyApps;
     private CheckBoxPreference mPowerButtonInstantlyLocks;
-
 
     private Preference mNotificationAccess;
 
@@ -186,7 +189,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
         }
         addPreferencesFromResource(resid);
 
-
         // Add options for device encryption
         mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
@@ -233,12 +235,25 @@ public class SecuritySettings extends RestrictedSettingsFragment
         mPowerButtonInstantlyLocks = (CheckBoxPreference) root.findPreference(
                 KEY_POWER_INSTANTLY_LOCKS);
 
+        PreferenceGroup securityCategory = (PreferenceGroup)
+                root.findPreference(KEY_SECURITY_CATEGORY);
+        if (securityCategory != null) {
+            Preference lockInterfacePref = findPreference(KEY_INTERFACE_SETTINGS);
+            Preference lockTargetsPref = findPreference(KEY_TARGET_SETTINGS);
+            if (lockInterfacePref != null && lockTargetsPref != null) {
+                if (!DeviceUtils.isPhone(getActivity())) {
+                     // Nothing for tablets and large screen devices
+                     securityCategory.removePreference(lockInterfacePref);
+                } else {
+                     securityCategory.removePreference(lockTargetsPref);
+                }
+            }
+        }
+
         // don't display visible pattern if biometric and backup is not pattern
         if (resid == R.xml.security_settings_biometric_weak &&
                 mLockPatternUtils.getKeyguardStoredPasswordQuality() !=
                 DevicePolicyManager.PASSWORD_QUALITY_SOMETHING) {
-            PreferenceGroup securityCategory = (PreferenceGroup)
-                    root.findPreference(KEY_SECURITY_CATEGORY);
             if (securityCategory != null && mVisiblePattern != null) {
                 securityCategory.removePreference(root.findPreference(KEY_VISIBLE_PATTERN));
             }
@@ -529,7 +544,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
 
-        updateBlacklistSummary();
+	updateBlacklistSummary();
     }
 
     @Override

src/com/android/settings/temasek/dslv/ButtonsListViewSettings.java
@@ -55,7 +55,7 @@ import android.widget.Toast;
 import com.android.internal.util.temasek.ButtonConfig;
 import com.android.internal.util.temasek.ButtonsConstants;
 import com.android.internal.util.temasek.ButtonsHelper;
-import com.android.internal.util.temasek.ColorHelper;
+import com.android.internal.util.temasek.ImageHelper;
 import com.android.internal.util.temasek.DeviceUtils;
 import com.android.internal.util.temasek.DeviceUtils.FilteredDeviceFeaturesArray;
 import com.android.internal.util.temasek.PolicyHelper;
@@ -197,7 +197,7 @@ public class ButtonsListViewSettings extends ListFragment implements
         mActionDialogValues = finalActionDialogArray.values;
         mActionDialogEntries = finalActionDialogArray.entries;
 
-        mPicker = new ShortcutPickerHelper(this, this);
+        mPicker = new ShortcutPickerHelper(mActivity, this);
 
         mImageTmp = new File(mActivity.getCacheDir()
                 + File.separator + "shortcut.tmp");
@@ -218,7 +218,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                         mPendingIndex = arg2;
                         mPendingLongpress = false;
                         mPendingNewButton = false;
-                        mPicker.pickShortcut();
+                        mPicker.pickShortcut(getId());
                     }
                 }
             }
@@ -236,7 +236,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                             mPendingIndex = arg2;
                             mPendingLongpress = true;
                             mPendingNewButton = false;
-                            mPicker.pickShortcut();
+                            mPicker.pickShortcut(getId());
                         }
                     }
                     return true;
@@ -290,7 +290,7 @@ public class ButtonsListViewSettings extends ListFragment implements
 
     @Override
     public void shortcutPicked(String action,
-                String description, Bitmap bmp, boolean isApplication) {
+                String description, boolean isApplication) {
         if (mPendingIndex == -1) {
             return;
         }
@@ -419,7 +419,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                         mPendingIndex = 0;
                         mPendingLongpress = false;
                         mPendingNewButton = true;
-                        mPicker.pickShortcut();
+                        mPicker.pickShortcut(getId());
                     }
                 }
                 break;
@@ -473,6 +473,8 @@ public class ButtonsListViewSettings extends ListFragment implements
             case POWER_MENU_SHORTCUT:
                 return PolicyHelper.getPowerMenuConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
+            case LOCKSCREEN_SHORTCUT:
+                return ButtonsHelper.getLockscreenShortcutConfig(mActivity);
         }
         return null;
     }
@@ -493,6 +495,8 @@ public class ButtonsListViewSettings extends ListFragment implements
                 break;
             case POWER_MENU_SHORTCUT:
                 PolicyHelper.setPowerMenuConfig(mActivity, buttonConfigs, reset);
+            case LOCKSCREEN_SHORTCUT:
+                ButtonsHelper.setLockscreenShortcutConfig(mActivity, buttonConfigs, reset);
                 break;
         }
     }
@@ -538,12 +542,12 @@ public class ButtonsListViewSettings extends ListFragment implements
                     + " " + getItem(position).getLongpressActionDescription());
             }
             if (mButtonMode == POWER_MENU_SHORTCUT) {
-                holder.iconView.setImageDrawable(ColorHelper.resize(
+                holder.iconView.setImageDrawable(ImageHelper.resize(
                         mActivity, PolicyHelper.getPowerMenuIconImage(mActivity,
                         getItem(position).getClickAction(),
                         getItem(position).getIcon(), false), 36));
             } else {
-                holder.iconView.setImageDrawable(ColorHelper.resize(
+                holder.iconView.setImageDrawable(ImageHelper.resize(
                         mActivity, ButtonsHelper.getButtonIconImage(mActivity,
                         getItem(position).getClickAction(),
                         getItem(position).getIcon()), 36));
@@ -729,7 +733,7 @@ public class ButtonsListViewSettings extends ListFragment implements
                                     getOwner().mPendingIndex = which;
                                     getOwner().mPendingLongpress = longpress;
                                     getOwner().mPendingNewButton = newButton;
-                                    getOwner().mPicker.pickShortcut();
+                                    getOwner().mPicker.pickShortcut(getId());
                                 }
                             } else {
                                 if (newButton) {

src/com/android/settings/temasek/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.temasek.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 0 : 1);
+        }
+        return true;
+    }
+}