4a070a84aa0d4ab2b2cae2b6b5d7f172ba76d1d0
Michael Bestas <mikeioannina@gmail.com>
2013-12-06 05:45:35 +0200

    Fix the build
    This shouldn't be there in the first place
    It's a result of a failed rebase
    
    Change-Id: I48610dfc4214a096b1c4af61af59bf52c5e50529

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -637,7 +637,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         resetRootAccessOptions();
         resetAdbNotifyOptions();
         resetVerifyAppsOverUsbOptions();
-        resetAdvancedRebootOptions();
         resetDevelopmentShortcutOptions();
         writeAnimationScaleOption(0, mWindowAnimationScale, null);
         writeAnimationScaleOption(1, mTransitionAnimationScale, null);
