99ba2b900999eaedc4c54884d35d1ecca139ae6e
Der-Schubi <schubi@erlangen.ccc.de>
2013-12-14 17:28:08 +0800

    [2/2] Settings: Pimp up ActiveDisplay Base part: https://gerrit.omnirom.org/#/c/2819/
    Change-Id: I8abf7a3018e7e0168860861ac240975cd1f8a2b0

--- src/com/android/settings/temasek/ActiveDisplaySettings.java
+++ src/com/android/settings/temasek/ActiveDisplaySettings.java
@@ -52,6 +52,7 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SHOW_DATE = "ad_show_date";
     private static final String KEY_SHOW_AMPM = "ad_show_ampm";
     private static final String KEY_BRIGHTNESS = "ad_brightness";
+    private static final String KEY_TIMEOUT = "ad_timeout";
 
     private SwitchPreference mEnabledPref;
     private CheckBoxPreference mShowTextPref;
@@ -64,6 +65,7 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mRedisplayPref;
     private SeekBarPreferenceChOS mBrightnessLevel;
     private AppMultiSelectListPreference mExcludedAppsPref;
+    private ListPreference mDisplayTimeout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -130,6 +132,13 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         Set<String> excludedApps = getExcludedApps();
         if (excludedApps != null) mExcludedAppsPref.setValues(excludedApps);
         mExcludedAppsPref.setOnPreferenceChangeListener(this);
+
+        mDisplayTimeout = (ListPreference) prefSet.findPreference(KEY_TIMEOUT);
+        mDisplayTimeout.setOnPreferenceChangeListener(this);
+        timeout = Settings.System.getLong(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_TIMEOUT, 8000L);
+        mDisplayTimeout.setValue(String.valueOf(timeout));
+        updateTimeoutSummary(timeout);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -153,6 +162,10 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             return true;
         } else if (preference == mExcludedAppsPref) {
             storeExcludedApps((Set<String>) newValue);
+            return true;
+        } else if (preference == mDisplayTimeout) {
+            long timeout = Integer.valueOf((String) newValue);
+            updateTimeoutSummary(timeout);
             return true;
         }
         return false;
@@ -210,6 +223,15 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         mRedisplayPref.setSummary(mRedisplayPref.getEntries()[mRedisplayPref.findIndexOfValue("" + value)]);
         Settings.System.putLong(getContentResolver(),
                 Settings.System.ACTIVE_DISPLAY_REDISPLAY, value);
+    }
+
+    private void updateTimeoutSummary(long value) {
+        try {
+            mDisplayTimeout.setSummary(mDisplayTimeout.getEntries()[mDisplayTimeout.findIndexOfValue("" + value)]);
+            Settings.System.putLong(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_TIMEOUT, value);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
     }
 
     private boolean hasProximitySensor() {
